a3fa10c055f66266643f78e55fe6c945f973be28
Patrick Leitermann <ptkltm.development@gmail.com>
2020-07-12 19:48:16 +0200

    The Gradle wrapper was upgraded from version 6.5 to 6.5.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -447,10 +447,10 @@ tasks {
     }
 
     /**
-     * Configures the Gradle wrapper with the version '6.5'.
+     * Configures the Gradle wrapper with the version '6.5.1'.
      */
     wrapper {
-        gradleVersion = "6.5"
+        gradleVersion = "6.5.1"
     }
 }
 
